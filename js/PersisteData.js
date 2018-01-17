9ef4edc60dc7cde8dd1c2ef7a603207dbee20be1
MayconVM <mayconvm@gmail.com>
2018-01-16 22:04:34 -0200

    Add methods read and clean data.

--- js/PersisteData.js
+++ js/PersisteData.js
@@ -5,7 +5,8 @@ class PersisteData {
   constructor() {
     this.db = new Dexie("tracker_database");
     this.db.version(1).stores({
-        tracker: 'end,start,url,domain'
+      tracker: 'end,start,url,domain',
+      firebase: 'apiKey,authDomain,databaseURL,projectId,storageBucket,messagingSenderId'
     });
 
     this.db.open().catch(function(error) {
@@ -25,6 +26,19 @@ class PersisteData {
       .then(resolve)
       .catch(reject);
     });
+  }
 
+  readData(store, condition) {
+      let data = this.db[store];
+
+      if (condition) {
+        data = data.where(condition);
+      }
+
+      return data.toArray();
+  }
+
+  cleanData(store) {
+    return this.db[store].clear();
   }
 }
