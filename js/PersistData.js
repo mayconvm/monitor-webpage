866facc18a3aa0825eef4832c7d547b6d5cfe518
MayconVM <mayconvm@gmail.com>
2018-01-21 17:07:47 -0200

    Method to writedata in firebase.

--- js/PersistData.js
+++ js/PersistData.js
@@ -2,7 +2,7 @@
 
 class PersistData {
 
-  constructor() {
+  constructor(firebase) {
     this.db = new Dexie("tracker_database");
     this.db.version(1).stores({
       tracker: 'end,start,url,domain',
@@ -10,9 +10,15 @@ class PersistData {
       user: 'id,email,token,displayName,photoURL'
     });
 
+    this.firebase = firebase;
+
+    this.dbFirebase = null;
+
     this.db.open().catch(function(error) {
       console.error('Uh oh : ' + error);
     });
+
+    this.initFirebase();
   }
 
   writeData(store, data) {
@@ -41,5 +47,72 @@ class PersistData {
 
   cleanData(store) {
     return this.db[store].clear();
+  }
+
+  initFirebase() {
+    let that = this;
+    this.readData('firebase').then((result) => {
+
+      let data = result[0];
+
+      var config = {
+        apiKey: data.apiKey,
+        authDomain: data.authDomain,
+        databaseURL: data.databaseURL,
+        projectId: data.projectId,
+        storageBucket: data.storageBucket,
+        messagingSenderId: data.messagingSenderId,
+      };
+      
+      that.firebase.initializeApp(config);
+    }).then(() => { 
+      // start persist database
+      that.initPersistDataFirebase();
+    });
+  }
+
+  initPersistDataFirebase() {
+    this.dbFirebase = this.firebase.database();
+  }
+
+  writeDataFirebase(store, data) {
+    if (!this.dbFirebase) {
+      throw new Error("Db Firebase not definded");
+    }
+
+    if (!data.length) {
+      return;
+    }
+
+    return new Promise((resolve, reject) => {
+      let promises = [];
+
+      for (let content of data) {
+        promises.push(this.dbFirebase.ref(store + "/" + this.getRandomId(5, 5)).set(content));
+      }
+
+      return Promise.all(promises);
+    })
+  }
+
+  getRandomId(length = 5, split = 1, separator = "-") {
+
+    let getRandomHash = function (length) {
+      let dataRandom = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm1234567890";
+      let r = '';
+      for(let i=0; length > i; i++) {
+        r += dataRandom[parseInt(Math.random() * 62)];
+      }
+
+      return r;
+    }
+
+    let r = '';
+    for (let i=0; split > i; i++) {
+      if (i) r += separator;
+      r += getRandomHash(length);
+    }
+
+    return r;
   }
 }
